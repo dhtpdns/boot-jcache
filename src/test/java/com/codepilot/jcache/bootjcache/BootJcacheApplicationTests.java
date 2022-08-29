bbabc89f85536c9d86f99db0af99cbd257c511ae
OSE <OSE@host.docker.internal>
2022-08-29 18:19:41 +0900

    first commit

--- src/test/java/com/codepilot/jcache/bootjcache/BootJcacheApplicationTests.java
+++ src/test/java/com/codepilot/jcache/bootjcache/BootJcacheApplicationTests.java
@@ -1,13 +1,13 @@
-package com.codepilot.jcache.bootjcache;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BootJcacheApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.codepilot.jcache.bootjcache;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class BootJcacheApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
