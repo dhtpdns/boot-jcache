def9c36caabfd2a7cfbabb3d592b9b5d1e8035d6
SCORE <SCORE@DESKTOP-NL8G6MJ>
2022-08-30 18:26:56 +0900

    commit

--- src/main/java/com/codepilot/jcache/bootjcache/BootJcacheApplication.java
+++ src/main/java/com/codepilot/jcache/bootjcache/BootJcacheApplication.java
@@ -5,9 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
 
 @EnableJpaRepositories
 @SpringBootApplication
+//@EnableSwagger2
 public class BootJcacheApplication {
 
 	public static void main(String[] args) {
